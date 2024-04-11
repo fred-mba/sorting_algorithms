476a3e7617f04e9c4f8b26f53aea391f8940b7f0
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-04-11 12:11:35 +0300

    Bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -13,6 +13,9 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j, temp;
 
+	if (array == NULL || size < 2)
+		return; /*return nothing*/
+
 	for (i = 0; i < (size - 1); i++)
 	{
 		for (j = 0; j < (size - 1); j++)
